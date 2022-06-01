35703a704db221484a769832588ab97bfe1d5386
Bryan Ayllon <bryanayllonroa@gmail.com>
2022-05-31 20:13:21 -0500

    Fixing Up Home Page

--- js/style.js
+++ js/style.js
@@ -1,1 +1,7 @@
-$('.alert').alert()
+// $( document ).ready(function() {
+//     $( ".btn-primary-outline" ).click(function() {
+//       $( ".btn-primary-outline" ).css('background', '#0A66C2');
+//       $( ".btn-primary-outline" ).css('color', 'white');
+
+//     });
+//   });
